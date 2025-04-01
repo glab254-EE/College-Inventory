d889c9771ccd30c0bf9e5221b12e9434585a5eca
glab254-EE <71868718+glab254-EE@users.noreply.github.com>
2025-04-01 11:01:59 +0300

    Added ability to safe to playerprefs for WEBGL.

--- Assets/Scripts/DataRelated/DataBase.cs
+++ Assets/Scripts/DataRelated/DataBase.cs
@@ -16,11 +16,19 @@ public class DataBase : MonoBehaviour
         try
         {
             Debug.Log("Loading init.");
+#if UNITY_EDITOR
             string path = Path.Combine(Application.persistentDataPath,fileName+".json");
             if (File.Exists(path))
             {
                 output = JsonConvert.DeserializeObject<SavableInventory>(File.ReadAllText(path));
             }
+#endif
+#if UNITY_WEBGL
+            if (PlayerPrefs.HasKey(fileName))
+            {
+                output = JsonConvert.DeserializeObject<SavableInventory>(PlayerPrefs.GetString(fileName));
+            }
+#endif
             Debug.Log("Loaded!");
         }
         catch (System.Exception e)
@@ -34,11 +42,17 @@ public class DataBase : MonoBehaviour
         try
         {
             Debug.Log("Saving init.");
+            string toWrite = JsonConvert.SerializeObject(toSafe);
+            #if UNITY_EDITOR
             string path = Path.Combine(Application.persistentDataPath,fileName+".json");
             if (File.Exists(path)) File.Delete(path);
             using StreamWriter writer = new(path,true);
-            string toWrite = JsonConvert.SerializeObject(toSafe);
             writer.Write(toWrite);
+            #endif
+            #if UNITY_WEBGL
+            PlayerPrefs.SetString(fileName,toWrite);
+            PlayerPrefs.Save();         
+            #endif
             Debug.Log("Saved!");
         }
         catch (System.Exception e)
